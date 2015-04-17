ec86552cbddd24198e35d60c2e87a6ef308befab
James Gosnell <jamesgosnell@gmail.com>
2015-04-17 01:34:35 -0500

    INITIAL commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/EhevuTov/node-intern-test.svg)](https://travis-ci.org/EhevuTov/node-intern-test)
+# Description
+A test repo for Intern using Node
+# Test
+`npm test`
---
+++ app/hello.js
@@ -0,0 +1,12 @@
+/*
+ *  * A very simple AMD module with no dependencies
+ *   */
+
+define([], function () {
+	return {
+		greet: function (name) {
+			name = name || 'world';
+			return 'Hello, ' + name + '!';
+		}
+	};
+});
