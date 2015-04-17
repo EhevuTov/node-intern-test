c2d0d496b62795337de22ec56327ec884227526d
James Gosnell <jamesgosnell@gmail.com>
2015-04-17 02:15:41 -0500

    REMOVED functional tests for now

--- tests/intern.js
+++ tests/intern.js
@@ -55,7 +55,7 @@ define({
 	suites: [ 'tests/hello'/* 'myPackage/tests/foo', 'myPackage/tests/bar' */ ],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ 'tests/functional/index' /* 'myPackage/tests/functional' */ ],
+	functionalSuites: [ /*'tests/functional/index', 'myPackage/tests/functional' */ ],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:tests|node_modules)\//
